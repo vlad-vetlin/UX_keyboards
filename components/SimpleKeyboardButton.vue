ec848970ba72fea86ee171842c5502d4b1b9d61e
green_tea <vetlin.vladislav@yandex.ru>
2020-03-08 17:28:39 +0300

    Add simple keyboard button

---
+++ components/SimpleKeyboardButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <div :class="getClassName">
+        {{ name }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SimpleKeyboardButton",
+        props: {
+            name: {type: String, required: true, },
+            isFocused: {type: Boolean, required: false, default: false},
+        },
+        computed: {
+            getClassName() {
+                if (this.isFocused) {
+                    return 'simple-keyboard-focused-button';
+                } else {
+                    return 'simple-keyboard-button';
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .simple-keyboard-button {
+        text-align: center;
+        padding: 1em;
+        border-style: solid;
+        border-color: black;
+        border-width: 1px;
+
+        width: 4em;
+        height: 1.5em;
+    }
+
+    .simple-keyboard-focused-button {
+        @extend .simple-keyboard-button;
+
+        border-width: 2px;
+        border-color: red;
+    }
+</style>
---
+++ pages/simpleKeyboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <simple-keyboard-button name="red-test"></simple-keyboard-button>
+        <simple-keyboard-button name="green-test" is-focused></simple-keyboard-button>
+    </div>
+</template>
+
+<script>
+    import SimpleKeyboardButton from "../components/SimpleKeyboardButton";
+    export default {
+        name: "SimpleKeyboard",
+        components: {SimpleKeyboardButton}
+    }
+</script>
+
+<style scoped>
+
+</style>
